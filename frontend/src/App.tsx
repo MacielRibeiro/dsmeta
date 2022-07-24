49810c9997e708ce7785fd959f4b6c528bf7d0a2
Maciel Ribeiro <92756410+MacielRibeiro@users.noreply.github.com>
2022-07-24 18:51:47 -0300

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
+
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App;
