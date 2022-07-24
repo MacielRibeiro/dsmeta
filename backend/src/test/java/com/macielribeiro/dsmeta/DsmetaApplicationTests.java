e43af045a15e6a851553e78f864bcd8b6196eee6
Maciel Ribeiro <92756410+MacielRibeiro@users.noreply.github.com>
2022-07-24 18:47:39 -0300

    Add files via upload

---
+++ backend/src/test/java/com/macielribeiro/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macielribeiro.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
